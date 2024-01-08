40fc38fc750d2d583b8e277bed9c45cd8703893b
GioacchinoArgo <gioacchinogabri.argo@libero.it>
2024-01-08 18:09:47 +0100

    completed javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,28 @@
 console.log('JS OK');
+
+// - 1
+
+const shoppingListElement = document.getElementById('shopping-list')
+
+// - 2 
+
+const shoppingList = ['pasta', 'patate', 'cipolle', 'carote', 'sedano','vino']
+
+// - 3
+
+let listItems = '<ul>'
+
+// - 4
+
+let i = 0
+while (i < shoppingList.length) {
+    listItems += `<li>${shoppingList[i]}</li>`
+    console.log(shoppingList[i])
+    i++;
+}
+
+listItems += '</ul>'
+
+// - 5 
+
+shoppingListElement.innerHTML = listItems
